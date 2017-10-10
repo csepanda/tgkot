85d2114b2182bc2c7ae93181990e085f9e546457
Andrey Bova <andrewbova@yandex.ru>
2017-10-10 04:34:33 +0300

    Add sending message container
    Add data-class which is designed to keep all message data in
    one object.

---
+++ src/main/kotlin/com/csepanda/tgkot/data/SMessage.kt
@@ -0,0 +1,46 @@
+package com.csepanda.tgbot.data
+
+/** Sending message container class.
+ *
+ *  @author  Andrey Bova
+ *  @version 0.1
+ *  @since   0.1
+ */
+class SMessage private constructor(chatIdNum : Int?, chatIdStr : String?, text: String) {
+    /** Unique identifier for the target chat or username of the target channel */
+    val chatId: Any = chatIdNum?: chatIdStr!!
+    /** Text of the message to be sent */
+    val text  : String = text
+
+    /** Make telegram render <code>Markdown</code> or <code>HTML</code> formatting */
+    var parseMode             : String?  = null; private set
+    /** Disables link previews for links in this message */
+    var disableWebPagePreview : Boolean? = null; private set
+    /** Sends the message silently. Users will receive a notification without sound */
+    var disableNotification   : Boolean? = null; private set
+    /** If the message is a reply, ID of the original message */
+    var replyToMessageId      : Int?     = null; private set
+
+    constructor(chatIdentifier: Int,    text: String) : this (chatIdentifier, null, text)
+    constructor(toUserName    : String, text: String) : this (null, toUserName, text)
+
+    constructor(chatId              : Int,      text                  : String,
+                parseMode           : String?,  disableWebPagePreview : Boolean?,
+                disableNotification : Boolean?, replyToMessageId      : Int?)
+            : this(chatId, text) {
+        this.parseMode             = parseMode
+        this.disableWebPagePreview = disableWebPagePreview
+        this.disableNotification   = disableNotification
+        this.replyToMessageId      = replyToMessageId
+    }
+
+    constructor(chatId              : String,   text                  : String,
+                parseMode           : String?,  disableWebPagePreview : Boolean?,
+                disableNotification : Boolean?, replyToMessageId      : Int?)
+            : this(chatId, text) {
+        this.parseMode             = parseMode
+        this.disableWebPagePreview = disableWebPagePreview
+        this.disableNotification   = disableNotification
+        this.replyToMessageId      = replyToMessageId
+    }
+}
